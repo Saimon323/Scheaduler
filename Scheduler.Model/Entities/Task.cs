cf089931bfc31ad75f07ca834fb083e174bd39e9
Saimon323 <szmiekus@gmail.com>
2013-12-09 00:06:21 +0100

    Dodanie podstawowej funkcjonalnoscie do Task repository

--- Scheduler.Model/Entities/Task.cs
+++ Scheduler.Model/Entities/Task.cs
@@ -8,5 +8,52 @@ namespace Scheduler.Model.EntityModels
 {
     public partial class Task :IIdentifable
     {
+        public Task()
+        {
+        }
+
+        public Task(int WorkerId, DateTime StartTime, DateTime StopTime, string TaskName, int Hours, int ProjectId)
+        {
+            this.id = 0;
+            this.WorkerId = WorkerId;
+            this.StartTime = StartTime;
+            this.StopTime = StopTime;
+            this.TaskName = TaskName;
+            this.Hours = Hours;
+            this.ProjectId = ProjectId;
+        }
+
+        public Task(int WorkerId, DateTime StartTime, string TaskName, int Hours, int ProjectId)
+        {
+            this.id = 0;
+            this.WorkerId = WorkerId;
+            this.StartTime = StartTime;
+            this.StopTime = null;
+            this.TaskName = TaskName;
+            this.Hours = Hours;
+            this.ProjectId = ProjectId;
+        }
+
+        public Task(DateTime StartTime, DateTime StopTime, string TaskName, int Hours, int ProjectId)
+        {
+            this.id = 0;
+            this.WorkerId = null;
+            this.StartTime = StartTime;
+            this.StopTime = StopTime;
+            this.TaskName = TaskName;
+            this.Hours = Hours;
+            this.ProjectId = ProjectId;
+        }
+
+        public Task(DateTime StartTime, string TaskName, int Hours, int ProjectId)
+        {
+            this.id = 0;
+            this.WorkerId = null;
+            this.StartTime = StartTime;
+            this.StopTime = null;
+            this.TaskName = TaskName;
+            this.Hours = Hours;
+            this.ProjectId = ProjectId;
+        }
     }
 }
--- Scheduler.Model/Repositories/TaskRepository.cs
+++ Scheduler.Model/Repositories/TaskRepository.cs
@@ -27,5 +27,147 @@ namespace Scheduler.Model.Repositories
         }
 
         #endregion
+
+        IUserRepository userRepo = new UserRepository();
+        IProjectRepository projectRepo = new ProjectRepository();
+        IGroupRepository groupRepo = new GroupRepository();
+
+        public Scheduler.Model.EntityModels.Task getTaskById(int id)
+        {
+            return Entities.Tasks.Where(x => x.id.Equals(id)).FirstOrDefault();
+        }
+
+        public Scheduler.Model.EntityModels.Task getTaskByNameAndProjectName(string ProjectName, string TaskName)
+        {
+            Project projectExist = projectRepo.getProjectByName(ProjectName);
+            if (ProjectName == null)
+                return null;
+
+            Scheduler.Model.EntityModels.Task task = Entities.Tasks.Where(x => x.ProjectId.Equals(projectExist.id) && x.TaskName.Equals(TaskName)).FirstOrDefault();
+            return task;
+        }
+
+        public void addNewTask(string Login, DateTime StartTime, DateTime StopTime, string TaskName, int Hours, string ProjectName)
+        {
+
+            Project projectExist = projectRepo.getProjectByName(ProjectName);
+            
+            if (projectExist == null)
+                return;
+
+            User userExist = userRepo.getUserByLogin(Login);
+
+            if (userExist == null || userExist.GroupId == null)
+                return;
+            
+            List<Group> GroupInProject = groupRepo.getAllGroupWorkingInProject(projectExist.ProjectName);
+
+            foreach (var x in GroupInProject)
+            {
+                if (userExist.GroupId == x.id)
+                {
+                    Scheduler.Model.EntityModels.Task task = new Scheduler.Model.EntityModels.Task(userExist.id, StartTime, StopTime, TaskName, Hours, projectExist.id);
+                    Entities.AddToTasks(task);
+                    Entities.SaveChanges();
+                }
+            }
+            
+        }
+
+        public void addNewTask(string Login, DateTime StartTime, string TaskName, int Hours, string ProjectName)
+        {
+
+            Project projectExist = projectRepo.getProjectByName(ProjectName);
+
+            if (projectExist == null)
+                return;
+
+            User userExist = userRepo.getUserByLogin(Login);
+
+            if (userExist == null || userExist.GroupId == null)
+                return;
+
+            List<Group> GroupInProject = groupRepo.getAllGroupWorkingInProject(projectExist.ProjectName);
+
+            foreach (var x in GroupInProject)
+            {
+                if (userExist.GroupId == x.id)
+                {
+                    Scheduler.Model.EntityModels.Task task = new Scheduler.Model.EntityModels.Task(userExist.id, StartTime, TaskName, Hours, projectExist.id);
+                    Entities.AddToTasks(task);
+                    Entities.SaveChanges();
+                }
+            }
+
+        }
+
+        public void addNewTask(DateTime StartTime, DateTime StopTime, string TaskName, int Hours, string ProjectName)
+        {
+
+            Project projectExist = projectRepo.getProjectByName(ProjectName);
+
+            if (projectExist == null)
+                return;
+
+
+            Scheduler.Model.EntityModels.Task task = new Scheduler.Model.EntityModels.Task(StartTime, StopTime, TaskName, Hours, projectExist.id);
+            Entities.AddToTasks(task);
+            Entities.SaveChanges();
+             
+
+        }
+
+        public void addNewTask(DateTime StartTime, string TaskName, int Hours, string ProjectName)
+        {
+
+            Project projectExist = projectRepo.getProjectByName(ProjectName);
+
+            if (projectExist == null)
+                return;
+
+            Scheduler.Model.EntityModels.Task task = new Scheduler.Model.EntityModels.Task(StartTime, TaskName, Hours, projectExist.id);
+            Entities.AddToTasks(task);
+            Entities.SaveChanges();
+
+
+        }
+
+        public void addUserToTask(string Login, string TaskName, string ProjectName)
+        {
+            Scheduler.Model.EntityModels.Task taskExist = getTaskByNameAndProjectName(ProjectName, TaskName);
+
+            User userExist = userRepo.getUserByLogin(Login);
+
+            if((taskExist == null && userExist == null) || userExist.GroupId == null)
+                return ;
+
+            bool realize = checkRealization(ProjectName, Login);
+
+            if (realize == false)
+                return;
+
+            taskExist.WorkerId = userExist.id;
+            Entities.SaveChanges();
+
+        }
+
+        public bool checkRealization(string ProjectName, string Login)
+        {
+            Project projectExist = projectRepo.getProjectByName(ProjectName);
+            User userExist = userRepo.getUserByLogin(Login);
+
+            if (projectExist != null && userExist != null && userExist.GroupId != null)
+                return false;
+
+            List<Group> groupList = groupRepo.getAllGroupWorkingInProject(projectExist.ProjectName);
+
+            foreach (var x in groupList)
+            {
+                if (userExist.GroupId == x.id)
+                    return true;
+            }
+
+            return false;
+        }
     }
 }
